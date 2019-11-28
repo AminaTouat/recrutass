30c19fbb4d557ed7672672147ac725256db70c38
sihem mansri <mansrisihem@gmail.com>
2019-11-28 15:20:32 +0100

    delet la nouvelle page d'acceuil

--- resources/views/post-jobs.blade.php
+++ resources/views/post-jobs.blade.php
@@ -12,11 +12,11 @@
                         <div class="custom-form">
                             <div id="message3"></div>
                             <form method="post" action="php/contact.php" name="contact-form" id="contact-form3">
-                                <h4 class="text-dark mb-3">Post a New Job :</h4>
+                                <h4 class="text-dark mb-3">Publier un nouvel emploi :</h4>
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Job Title</label>
+                                            <label class="text-muted">Profession</label>
                                             <input id="company-name" type="text" class="form-control resume" placeholder="">
                                         </div>
                                     </div>
@@ -25,26 +25,26 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Job Type</label>
+                                            <label class="text-muted">Type d'emploi</label>
                                             <div class="form-button">
                                                 <select class="nice-select rounded">
-                                                    <option data-display="Job Type">Job Type</option>
-                                                    <option value="1">Full Time</option>
-                                                    <option value="2">Part Time</option>
+                                                    <option data-display="Job Type">Type d'emploi</option>
+                                                    <option value="1">À plein temps</option>
+                                                    <option value="2">À temps partiel</option>
                                                 </select>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Job Category</label>
+                                            <label class="text-muted">catégorie d'emploi</label>
                                             <div class="form-button">
                                                 <select class="nice-select rounded">
-                                                    <option data-display="Category">Category</option>
-                                                    <option value="1">Web Developer</option>
-                                                    <option value="2">PHP Developer</option>
-                                                    <option value="3">Web Designer</option>
-                                                    <option value="4">Graphic Designer</option>
+                                                    <option data-display="Category">Catégorie</option>
+                                                    <option value="1">Développeur web</option>
+                                                    <option value="2">Développeur PHP</option>
+                                                    <option value="3">Concepteur web</option>
+                                                    <option value="4">Designer graphique</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -54,13 +54,13 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">City</label>
+                                            <label class="text-muted">Ville</label>
                                             <input id="city" type="text" class="form-control resume" placeholder="">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Country</label>
+                                            <label class="text-muted">Pays</label>
                                             <div class="form-button">
                                                 <select class="nice-select rounded">
                                                     <option data-display="Country">Country</option>
@@ -77,13 +77,13 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Minimum Salary</label>
+                                            <label class="text-muted">Salaire minimum</label>
                                             <input id="minimum-salary" type="text" class="form-control resume" placeholder="$8000">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Maximum Salary</label>
+                                            <label class="text-muted">Salaire Maximum</label>
                                             <input id="maximum-salary" type="text" class="form-control resume" placeholder="$20000">
                                         </div>
                                     </div>
@@ -92,14 +92,14 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Education Level</label>
+                                            <label class="text-muted">niveau d'éducation</label>
                                             <div class="form-button">
                                                 <select class="nice-select rounded">
                                                     <option data-display="Level">Level</option>
-                                                    <option value="1">Level-1</option>
-                                                    <option value="2">Level-2</option>
-                                                    <option value="3">Level-3</option>
-                                                    <option value="4">Level-4</option>
+                                                    <option value="1">niveau-1</option>
+                                                    <option value="2">niveau-2</option>
+                                                    <option value="3">niveau-3</option>
+                                                    <option value="4">niveau-4</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -107,13 +107,13 @@
 
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Year of Experience</label>
+                                            <label class="text-muted">Année d'expérience</label>
                                             <div class="form-button">
                                                 <select class="nice-select rounded">
                                                     <option data-display="Experience">Experience</option>
-                                                    <option value="1">1 Year</option>
-                                                    <option value="2">2 Year</option>
-                                                    <option value="3">3 Year</option>
+                                                    <option value="1">1 année</option>
+                                                    <option value="2">2 année</option>
+                                                    <option value="3">3 année</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -123,7 +123,7 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Website</label>
+                                            <label class="text-muted">Site Internet</label>
                                             <input id="url" type="url" class="form-control resume" placeholder="">
                                         </div>
                                     </div>
@@ -132,14 +132,14 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Email Address</label>
+                                            <label class="text-muted">Adresse e-mail</label>
                                             <input id="email-address" type="text" class="form-control resume" placeholder="">
                                         </div>
                                     </div>
 
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Phone Number</label>
+                                            <label class="text-muted">Numéro de téléphone</label>
                                             <input id="number" type="text" class="form-control resume" placeholder="">
                                         </div>
                                     </div>
@@ -148,12 +148,12 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Gender</label>
+                                            <label class="text-muted">Le genre</label>
                                             <div class="form-button">
                                                 <select class="nice-select rounded">
-                                                    <option data-display="Gender">Gender</option>
-                                                    <option value="1">Male</option>
-                                                    <option value="2">Female</option>
+                                                    <option data-display="Gender">Le genre</option>
+                                                    <option value="1">Mâle</option>
+                                                    <option value="2">Femelle</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -161,12 +161,12 @@
 
                                     <div class="col-md-6">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Shift</label>
+                                            <label class="text-muted">Décalage</label>
                                             <div class="form-button">
                                                 <select class="nice-select rounded">
-                                                    <option data-display="Shift">Shift</option>
-                                                    <option value="1">Morning</option>
-                                                    <option value="2">Evening</option>
+                                                    <option data-display="Shift">Décalage</option>
+                                                    <option value="1">Matin</option>
+                                                    <option value="2">Soir</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -176,7 +176,7 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group app-label mt-2">
-                                            <label class="text-muted">Job Description</label>
+                                            <label class="text-muted">Description de l'emploi</label>
                                             <textarea id="description" rows="6" class="form-control resume" placeholder=""></textarea>
                                         </div>
                                     </div>
@@ -195,7 +195,7 @@
                                             </li>
 
                                             <li class="list-inline-item">
-                                                <h6 class="text-muted mb-0">Upload Images Or Documents.</h6>
+                                                <h6 class="text-muted mb-0">Télécharger des images ou des documents.</h6>
                                             </li>
                                         </ul>
                                     </div>
